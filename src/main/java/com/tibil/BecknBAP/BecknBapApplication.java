39cfc0241a53e1e2d1ee5bc24f7d2e083c2273d5
Sanjay-krishnan <sanjay.krishnan@tibilsolutions.com>
2023-03-06 16:41:36 +0530

    Initial commit

---
+++ src/main/java/com/tibil/BecknBAP/BecknBapApplication.java
@@ -0,0 +1,13 @@
+package com.tibil.BecknBAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecknBapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecknBapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tibil/BecknBAP/BecknBapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibil.BecknBAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecknBapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
